55d55038c69c18bc9d98de2faeffeac851d4522d
Bakhtjan <108337927+Bakhtjan@users.noreply.github.com>
2022-08-13 16:47:48 +0500

    Добавьте файлы проекта.

---
+++ tipygaming2/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace tipygaming2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Orange;
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Location = new System.Drawing.Point(1, 648);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(938, 100);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.MidnightBlue;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.Orange;
+            this.label1.Location = new System.Drawing.Point(1, -1);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 80);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "a";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Navy;
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Location = new System.Drawing.Point(294, 192);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(323, 277);
+            this.panel2.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.button1.Font = new System.Drawing.Font("Microsoft YaHei", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Location = new System.Drawing.Point(107, 116);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 48);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            this.button1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.button1_KeyPress_1);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick_1);
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(847, 31);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 37);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "00:00";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(941, 748);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ tipygaming2/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tipygaming2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Random randomL = new Random();
+        Random randomR = new Random();
+        int x = 0;
+        int y = 0;
+        char a = '@';
+        private void manis_beriw()
+        {
+            x = randomL.Next(1, 700);
+            a = (char)randomR.Next(97, 122);
+            label1.Location = new Point(x, 0);
+            label1.Text = a.ToString();
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void guna2CustomGradientPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+     /*   private void button1_Click(object sender, EventArgs e)
+        {
+            panel2.Visible = false;
+            manis_beriw();
+            timer1.Start();
+
+
+        }*/
+
+       /* private void button1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar.ToString() == label1.Text)
+            {
+                manis_beriw();
+                y = 0;
+            }
+
+        }*/
+        int minut = 0;
+        int sekunt = 0;
+       /* private void timer1_Tick(object sender, EventArgs e)
+        {
+            y += 5;
+            label1.Location = new Point(x, y);
+            if (label1.Location.Y > 530)
+            {
+                manis_beriw();
+                y = 0;
+            }
+
+            if (sekunt == 60)
+            {
+                minut++;
+                sekunt = 0;
+            }
+            else
+            {
+                sekunt++;
+            }
+
+            label3.Text = "00" + ":" + "" + minut.ToString();
+        }*/
+
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void backgroundWorker1_DoWork(object sender, System.ComponentModel.DoWorkEventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+            y += 8;
+            label1.Location = new Point(x, y);
+            if (label1.Location.Y > 460)
+            {
+                manis_beriw();
+                y = 0;
+            }
+
+            if (sekunt == 60)
+            {
+                minut++;
+                sekunt = 0;
+            }
+            else
+            {
+                sekunt++;
+            }
+
+            label3.Text = "00" + ":" + "" + minut.ToString();
+        }
+
+        private void button1_KeyPress_1(object sender, KeyPressEventArgs e)
+        {
+            {
+                if (e.KeyChar.ToString() == label1.Text)
+                {
+                    manis_beriw();
+                    y = 0;
+                }
+
+            }
+            
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+            panel2.Visible = false;
+            manis_beriw();
+            timer1.Start();
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
